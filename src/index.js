7e1e785f6d56c3233a121632ca3e689d889d7f8d
Сағиден Нариман Тұрсынғалиұлы <saghiden.n@psu.local>
2022-06-22 10:56:08 +0600

    checking

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { store } from './store';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render( 
   <Provider store={store}>
     <App />
   </Provider>
